cdcdf0c3e35b0f2b2ee246c7369d09367ce13618
irvan <irvandta@gmail.com>
2022-01-05 13:10:56 +0700

    update

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -25,7 +25,12 @@ class CorsMiddleware
         if ($request->isMethod('OPTIONS')) {
             return response()->json('{"method":"OPTIONS"}', 200, $headers);
         }
-        $response = $next($request);
+        try {
+            $response = $next($request);
+        } catch (\Throwable $th) {
+            return response()->json(404, $headers);
+        }
+
         foreach ($headers as $key => $row) {
             $response->header($key, $row);
         }
